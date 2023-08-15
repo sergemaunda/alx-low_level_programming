a872e83ea53a2cb74ef75a7450f98740e221c48a
sergemaunda <serge.maunda.3@gmail.com>
2023-08-15 01:46:05 -0700

    Task 1: prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: noting.
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; ++i)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
