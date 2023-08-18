e0224c5e9d1873a5432d2ad8ccb538df72abc710
sergemaunda <serge.maunda.3@gmail.com>
2023-08-18 04:46:45 -0700

    Hello World Tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - starting point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59, 1, stderr);
+	exit(EXIT_FAILURE);
+}
