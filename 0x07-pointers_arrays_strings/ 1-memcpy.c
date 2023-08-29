023f500473676206fc8e5dbedbd23060eb46ebe1
sergemaunda <serge.maunda.3@gmail.com>
2023-08-29 03:33:50 +0000

    Task 1

---
+++ 0x07-pointers_arrays_strings/ 1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+* _memcpy -copies n bytes from memory area src to memory area dest
+* @dest: memory area destination
+* @src: memory area source
+* @n: number of bytes.
+* Return: a pointer to dest.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    while (n > 0) {
+        dest[n - 1] = src[n - 1];
+        n--;
+    }
+    return dest;
+}
