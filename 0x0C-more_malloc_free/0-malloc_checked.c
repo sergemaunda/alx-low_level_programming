ec691f3ffdafb89d20a86beb3292dd984adfa67e
sergemaunda <serge.maunda.3@gmail.com>
2023-11-24 13:14:25 +0000

    Task 0 - Corrections

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,15 @@
 
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL)
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
 	{
 		exit(98);
-	} else
-	{
-		return (malloc(b));
 	}
+
+	return (malloc(b));
 }
 
